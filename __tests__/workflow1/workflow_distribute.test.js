6f85045788cb21fa803fc6b49f9bf789d01e8db8
Byran <sunhaolin@hotoa.com>
2023-02-28 18:37:26 +0800

    添加分发测试文件、调整浏览器打开窗口大小

---
+++ __tests__/workflow1/workflow_distribute.test.js
@@ -0,0 +1,60 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2023-02-28 11:56:46
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2023-02-28 18:13:44
+ * @Description: 
+ */
+const puppeteer = require('puppeteer')
+
+const timeout = 300000;
+
+
+test('page title', async () => {
+    const browser = await puppeteer.launch(require('../../jest-puppeteer.config').launch)
+    const promises = []
+    for (let index = 0; index < 2; index++) {
+        promises.push(login(browser))
+    }
+    await Promise.all(promises)
+}, timeout);
+
+async function login(browser) {
+    console.log('login')
+    const page = await browser.newPage();
+
+    // Go to your site
+    await page.goto(STEEDOS_ROOT_URL);
+
+    await new Promise(r => setTimeout(r, 2000));
+
+    const title = await page.title();
+    expect(title).toBe('登录您的账户 | 我的公司');
+
+    const headerOne = await page.$('h2');
+    const header = await page.evaluate(headerOne => headerOne.innerHTML, headerOne);
+    expect(header).toBe("登录您的账户");
+
+    await page.type('#loginId', STEEDOS_USERNAME);
+    await page.type('#password', STEEDOS_PASSWORD);
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('[type=submit]')
+    await new Promise(r => setTimeout(r, 2000));
+
+
+    // // 选择并点击第一个企业
+    // try {
+    //     let xPath = '//*[@id="root"]/div/div[3]/div/div[2]/ul/li[1]';
+    //     await page.waitForXPath(xPath);
+    //     let elements = await page.$x(xPath);
+    //     elements[0].click();
+    // } catch (error) {}
+    await new Promise(r => setTimeout(r, 3000));
+
+    // await distribute(page);
+    await page.goto(`${STEEDOS_ROOT_URL}/workflow`)
+}
+
+async function distribute(p) {
+    await p.goto(`${STEEDOS_ROOT_URL}/workflow`)
+}
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  launch: { 
-    headless: process.env.PUPPETEER_HEADLESS === 'true' ? true: false, 
+  launch: {
+    headless: process.env.PUPPETEER_HEADLESS === 'true' ? true : false,
     args: [
-        '--lang=zh-CN', 
-        '-–disable-gpu', // GPU硬件加速
-        '-–disable-dev-shm-usage', // 创建临时文件共享内存
-        '-–disable-setuid-sandbox', // uid沙盒
-        '-–no-first-run', // 没有设置首页。在启动的时候，就会打开一个空白页面。
-        '-–no-sandbox', // 沙盒模式
-        '-–no-zygote',
-        '-–single-process' // 单进程运行
+      '--lang=zh-CN',
+      '-–disable-gpu',            // GPU硬件加速
+      '-–disable-dev-shm-usage',  // 创建临时文件共享内存
+      '-–disable-setuid-sandbox', // uid沙盒
+      '-–no-first-run',           // 没有设置首页。在启动的时候，就会打开一个空白页面。
+      '-–no-sandbox',             // 沙盒模式
+      '-–no-zygote',
+      '-–single-process',         // 单进程运行
+      '--window-size=1920,1080',  // 控制浏览器窗口大小
     ],
-    slowMo: 0, 
-  } 
+    defaultViewport: {              // 控制浏览器内窗口大小
+      width: 1920,
+      height: 1080
+    },
+    slowMo: 0,
+    ignoreHTTPSErrors: true,
+  }
 }
