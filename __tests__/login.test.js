96ae2f8376724dd8be65ed1fd1cdba21bc65570b
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-02-28 15:58:16 +0800

    test apps

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -10,14 +10,14 @@ describe('Login', () => {
   describe('Test page title and header', () => {
     test('page title', async () => {
       const title = await page.title(); 
-      expect(title).toBe('登录您的账户 | 华炎软件'); 
+      expect(title).toContain('登录您的账户'); 
     }, timeout); 
     test('Header', async () => { 
       const headerOne = await page.$('h2'); 
       const header = await page.evaluate(headerOne => headerOne.innerHTML, headerOne); 
       expect(header).toBe("登录您的账户"); 
     }, timeout); 
-    test('Login', async () => { 
+    test('LoginFailed', async () => { 
       await page.type('#loginId', STEEDOS_USERNAME);
       await page.type('#password', 'bad-password');
       await page.waitForTimeout(1000);
