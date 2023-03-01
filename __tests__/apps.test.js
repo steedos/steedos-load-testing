95a8ee1b0d2290af4fad49425745695bc3d6f40e
何夏鹏 <hexiapeng@steedos.com>
2023-03-01 14:12:14 +0800

    Update apps.test.js

--- __tests__/apps.test.js
+++ __tests__/apps.test.js
@@ -21,14 +21,22 @@ describe('Browse Apps', () => {
     expect(appListBtn).toBeDefined();
   }, timeout); 
 
+  
   test('Apps', async () => { 
-    const page = await browser.newPage();
-    await page.goto(STEEDOS_ROOT_URL);
-    await page.waitForNavigation();
-    await page.waitForSelector('.steedos-header-container');
+
+
+    // 设置开启多少个浏览器
+    var round_apps = 50;
+    for(var i = 0; i < round_apps; i ++ ) {
+      page = await browser.newPage();
+      await page.goto(STEEDOS_ROOT_URL);
+      await page.waitForNavigation();
+      await page.waitForSelector('.steedos-header-container');
+    }
     await page.waitForSelector('.steedos-header-container2', {timeout});
     const appListBtn = await page.$('.steedos-header-container');
     expect(appListBtn).toBeDefined();
   }, timeout); 
+
 
 });
