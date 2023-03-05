1e26b9b65d30940c90f958b326c62ecdf249ff54
何夏鹏 <hexiapeng@steedos.com>
2023-03-05 15:52:18 +0800

    Update approve2.test.js

--- __tests__/approve2.test.js
+++ __tests__/approve2.test.js
@@ -2,7 +2,7 @@
  * @Author: 何夏鹏 hexiapeng@steedos.com
  * @Date: 2023-03-01 15:35:18
  * @LastEditors: 何夏鹏 hexiapeng@steedos.com
- * @LastEditTime: 2023-03-01 17:53:12
+ * @LastEditTime: 2023-03-05 14:52:42
  * @FilePath: /steedos-load-testing/__tests__/approve2.test.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,7 +21,7 @@ describe('Login And Approve', () => {
     await page.type('#loginId', STEEDOS_USERNAME);
     await page.evaluate(() => password.value = "", password); 
     await page.type('#password', STEEDOS_PASSWORD);
-    await page.click('[type=submit]')
+    await page.click('[type=submit]');
     const appListBtn = await page.$('.steedos-header-container'); 
     expect(appListBtn).toBeDefined();
   }, timeout);
@@ -30,9 +30,9 @@ describe('Login And Approve', () => {
     await click_delay('[title="审批单"]', 7000);
     await click_delay('.steedos-button-instances-instance_new');
     await click_delay('[title="集团公司收文"]', 7000);
-    await write_delay('[title="来文单位"]', 7000);
-    await write_delay('[title="文件标题"]');
-    await choose_delay('[title="所属部门"]');
+    await write_delay('[title="来文单位"]', '同意', 7000);
+    await write_delay('[title="文件标题"]', '同意');
+    await write_delay('[title="所属部门"]', '集团办公室');
     await click_delay('.nextStepUsers');
     await click_delay('.user-name');
     await click_delay('[title="发送"]');
@@ -43,24 +43,22 @@ describe('Login And Approve', () => {
 
 
 
+// 延时后点击
 async function click_delay(str, time = 3000){
-  /* 
-    str 抓取的元素
-  */
   await page.waitForTimeout(time);
   await page.click(str);
 };
 
-async function write_delay(str, time = 3000){
+/* 
+  延时后可以进行 选择\写入
+  str 选择元素
+  value 传入的值
+  time 延时时间，可以不传
+*/
+async function write_delay(str, content, time = 3000){
+  
   await page.waitForTimeout(time);
-  await page.$eval(str, (el) => {
-    el.value = '同意';
-})
+  await page.$eval(str, (el, fillContent) => {
+    el.value = fillContent;
+  }, content);
 };
-
-async function choose_delay(str, time = 3000){
-  await page.waitForTimeout(time);
-  await page.$eval(str, (el) => {
-    el.value = '集团办公室';
-  });
-}
