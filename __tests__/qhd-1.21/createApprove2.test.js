4583a3dcf7a53105d7d2e01e5efc867a4df02190
何夏鹏 <hexiapeng@steedos.com>
2023-03-05 16:02:12 +0800

    Create createApprove2.test.js

---
+++ __tests__/qhd-1.21/createApprove2.test.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: 何夏鹏 hexiapeng@steedos.com
+ * @Date: 2023-03-05 11:47:37
+ * @LastEditors: 何夏鹏 hexiapeng@steedos.com
+ * @LastEditTime: 2023-03-05 16:01:28
+ * @FilePath: /steedos-load-testing/__tests__/qhd-1.21/createApprove.test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+const timeout = 3000000; 
+
+// 如果网络不好，会出现问题
+describe('Login And Approve', () => {
+    
+  beforeAll(async () => {
+
+  }, timeout);
+
+
+  test('Login', async () => {
+    let num = 99;
+
+    // Login
+    console.log("--------Login--------");
+    await page.goto(STEEDOS_ROOT_URL, {waitUntil: 'networkidle0'});
+    await page.waitForSelector('#at-field-username_and_email');
+    await page.type('#at-field-username_and_email', STEEDOS_USERNAME);
+    await page.type('#at-field-password', STEEDOS_PASSWORD);
+    await click_delay('[type=submit]', 1000);
+
+    for( let i = 0; i < num; i ++ ) {
+        await fun();
+        await console.log( '--------当前浏览器数量--------:' + i + 1 );
+    }
+    await page.waitForSelector('.steedos99999', {timeout});
+    
+  }, timeout); 
+
+});
+
+
+
+
+// 延时后点击
+async function click_delay(str, time = 3000){ 
+//   await page.waitForSelector(str);
+  await page.waitForTimeout(time);
+  await page.$eval(str, (el) => {
+        el.click();
+    });
+};
+
+/* 
+  延时后可以进行 选择\写入
+  str 选择元素
+  value 传入的值
+  time 延时时间，可以不传
+*/
+async function write_delay(str, content, time = 3000){
+//   await page.waitForSelector(str);
+  await page.waitForTimeout(time);
+  await page.$eval(str, (el, fillContent) => {
+    el.value = fillContent;
+  }, content);
+};
+
+// 重复提交公文
+async function fun(){
+    // gotoWorkflow
+    console.log("--------gotoWorkflow--------");
+    await page.waitForSelector('[data-appid="workflow"]');
+    await click_delay('[data-appid="workflow"]');
+    // newFile
+    console.log("--------newFile--------");
+    await page.waitForSelector('.instance_new');
+    await click_delay('.instance_new', 1000);
+    await click_delay('[data-flow="1ff12bc17e235503aff2c4c9"]');
+    await write_delay('[name="发文类型"]', '政务发文');
+    await write_delay('[name="主送"]', '测试');
+    await write_delay('[title="标题"]', '测试');
+    await click_delay('[value="a005e57c-ecf0-4aa0-ac8d-a90a76c0b56f"]');
+    await click_delay('.selectUser-placeholder'); // 点击选择秘书 
+    await click_delay('#EayxjWDetqaP8BjP6'); // 选人 王海滨
+    await click_delay('.fa-paper-plane'); // 发送
+    page = await browser.newPage();
+    await page.goto(STEEDOS_ROOT_URL);
+    await page.waitForSelector('[data-appid="workflow"]');
+}
